ef0446b18c38bd39bef6482e13b52b1f45818209
Dan Molina <digzmol45@gmail.com>
2018-02-14 19:32:44 -0800

    closing remark

--- my_App/my_App/main/main.py
+++ my_App/my_App/main/main.py
@@ -8,6 +8,7 @@ def main():
 			scraper.dataExtraction("../inputFiles/para0" + str(i) + ".pdf")  #path strings to the pdfs
 
 	scraper.writeToExcel("../outputFiles/BeneFix Small Group Plans upload template.xlsx") #path string to the excel file
+	print("The scraping is done!! Check the excel file")
 
 if __name__ == '__main__':
 	main()
